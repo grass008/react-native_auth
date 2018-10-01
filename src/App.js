5ebcb2e49cfcb34c015c5b649368cf22c6694de2
Yura Trotskyi <>
2018-10-01 13:30:28 +0300

    created loginForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase'
+import { Header } from './components/common'
+// import LoginForm from '/.components/LoginForm'
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+    		apiKey: 'AIzaSyCVCnthh1DF_YEUf4jknWrOZiiApUW1Zgk',
+    		authDomain: 'auth-be421.firebaseapp.com',
+    		databaseURL: 'https://auth-be421.firebaseio.com',
+    		projectId: 'auth-be421',
+    		storageBucket: 'auth-be421.appspot.com',
+    		messagingSenderId: '839045318574'
+  		});
+	}
+
 	render() {
 		return (
 				<View>
-					<Text>An app</Text>
+					// <Header headerText="Authentication" />
+					<Text > what a shit?? </Text>
 				</View>
 			);
 	}
---
+++ src/components/LoginForm.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'React';
+import { View, Text } from 'react-native';
+// import { Button, Card, CardSection } from './common'
+
+class LoginForm extends Component {
+	render() {
+		return (
+			<View>
+				<Text> So? </Text>
+			</View>
+			// <Card>
+			// 	<CardSection />
+			// 	<CardSection />
+
+			// 	<CardSection>
+			// 		<Button>
+			// 			Log In
+			// 		</Button>
+			// 	</CardSection>
+			// </Card>
+			)
+	}
+}
+
+export default LoginForm;
