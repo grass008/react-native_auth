d6f8d7cabc1f00c9a71c43f574d43aa848ac19f8
Yura Trotskyi <>
2018-10-01 10:40:33 +0300

    added common files

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-App.js
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+	render() {
+		return (
+				<View>
+					<Text>An app</Text>
+				</View>
+			);
+	}
+}
+
+export default App;
